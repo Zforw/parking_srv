f8fb6392e9a15287796d97e1482bb6eba8a761eb
Zforw <oncwnuPRFHL864LbTExMPm7ZOAVg@git.weixin.qq.com>
2023-03-16 14:23:26 +0800

    实现了license、user的添加

--- tests/test_license.go
+++ tests/test_license.go
@@ -1,17 +1,63 @@
 package tests
 
 import (
+	"fmt"
 	"go.uber.org/zap"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+	"log"
+	"os"
 	"parking/global"
-	"parking/handler"
 	"parking/model"
 	"testing"
+	"time"
 )
+
+var DB *gorm.DB
+
+func init() {
+	c := global.ServerConfig.MySqlInfo
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		c.User, c.Pass, c.Host, c.Port, c.DB)
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second,   // 慢 SQL 阈值
+			LogLevel:      logger.Silent, // Log level
+			Colorful:      true,          // 禁用彩色打印
+		},
+	)
+
+	// 全局模式
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestCreateUser(t *testing.T) {
+	user := &model.User{
+		OpenId: "ow6HF5Y8rSlm61hl8igL1k9nOlAI",
+	}
+	err := DB.Create(&user)
+	if err != nil {
+		zap.S().Fatal(err)
+		return
+	}
+}
 
 func TestCreateLicense(t *testing.T) {
 	var user model.User
 	user.OpenId = "ow6HF5Y8rSlm61hl8igL1k9nOlAI"
-	if result := global.DB.First(&user); result.RowsAffected == 0 {
+	if result := DB.First(&user); result.RowsAffected == 0 {
 		zap.S().Debug("用户不存在")
 		return
 	}
@@ -21,7 +67,7 @@ func TestCreateLicense(t *testing.T) {
 		User:   user,
 		Status: "OUT",
 	}
-	err := handler.CreateLicense(license)
+	err := DB.Create(&license)
 	if err != nil {
 		zap.S().Fatal(err)
 		return
