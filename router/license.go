646e135511347a88d1c57d241479401f082ddd1b
Zforw <oncwnuPRFHL864LbTExMPm7ZOAVg@git.weixin.qq.com>
2023-03-20 21:26:07 +0800

    登录（token测试） 待完成：获取用户车牌列表、停车区

--- router/license.go
+++ router/license.go
@@ -4,15 +4,14 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 	"parking/api"
-	"parking/middlewares"
 )
 
 func InitLicenseRouter(group *gin.RouterGroup) {
 	LicenseRouter := group.Group("l")
 	zap.S().Debug("配置车牌相关的URL")
 	{
-		LicenseRouter.POST("add", middlewares.JWTAuth(), api.CreateLicense)
-		LicenseRouter.POST("update", middlewares.JWTAuth(), api.UpdateLicense)
+		LicenseRouter.POST("add", api.CreateLicense)
+		LicenseRouter.POST("update", api.UpdateLicense)
 		LicenseRouter.GET("list", api.GetLicenseList)
 		LicenseRouter.GET("ulist", api.GetUserLicenseList)
 	}
--- router/spot.go
+++ router/spot.go
@@ -4,14 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 	"parking/api"
+	"parking/middlewares"
 )
 
 func InitSpotRouter(group *gin.RouterGroup) {
 	SpotRouter := group.Group("s")
 	zap.S().Debug("配置停车位相关的URL")
 	{
-		SpotRouter.POST("add", api.CreateSpot)
-		SpotRouter.POST("update", api.UpdateSpot)
-		SpotRouter.GET("list", api.GetSpotList)
+		SpotRouter.POST("add", middlewares.JWTAuth(), api.CreateSpot)
+		SpotRouter.POST("update", middlewares.JWTAuth(), api.UpdateSpot)
+		SpotRouter.GET("list", middlewares.JWTAuth(), api.GetSpotList)
 	}
 }
